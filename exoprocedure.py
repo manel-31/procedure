69260d9e22a12c72489b012fefbbe80dc6c6b071
manel amoura <manelamoura@MacBook-Pro-de-manel.local>
2020-08-11 00:42:33 +0100

    pour tester

---
+++ exoprocedure.py
@@ -0,0 +1,11 @@
+#gardien de phare
+
+#definition de la fonction
+def hauteurparcourue(nbr,h):
+    print('pour',nbr,' marches de ',h,' cm , il parcoure',(nbr*h*2*5*7)/100,' m par semaine')
+
+#le programme principal
+nbr=float(input('entez le nombre de pas :'))
+h=float(input('entrez la hauteur des pas '))
+
+hauteurparcourue(nbr,h)
